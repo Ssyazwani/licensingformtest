9347dec0b1141bbf3e414e4bbb1f7b14fbb06f0b
Ssyazwani <sitisyazwanibtemaziz@gmail.com>
2025-01-14 18:31:51 +0800

    adding validations to form and dependency

--- demo/demo/src/main/java/licensingapp/demo/model/FormData.java
+++ demo/demo/src/main/java/licensingapp/demo/model/FormData.java
@@ -2,14 +2,29 @@ package licensingapp.demo.model;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
 public class FormData {
+
+     @NotNull(message = "Name of Company cannot be null")
     private String name;
+
+    @NotNull(message = "UEN cannot be null")
     private String uen;
+
+    @NotNull(message = "Type cannot be null,MPI,SPI or MC")
     private String typeOfApplication;
+
+  
+    @NotNull(message = "Date cannot be null")
+    @Pattern(regexp = "^(0[1-9]|[12][0-9]|3[01])(0[1-9]|1[0-2])\\d{4}$", 
+             message = "Start date must be in DDMMYYYY format")
     private LocalDate dateOfApplication;
+
+    @NotNull(message = "Activities cannot be null")
     private String activities;
 
-    // Getters and Setters
     public String getName() {
         return name;
     }
