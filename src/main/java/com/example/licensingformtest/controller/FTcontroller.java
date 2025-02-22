89756600e244e1fd5a40ee9b95f5920ac3e5c844
Ssyazwani <sitisyazwanibtemaziz@gmail.com>
2025-02-22 16:23:38 +0800

    trying to make validations

---
+++ src/main/java/com/example/licensingformtest/controller/FTcontroller.java
@@ -0,0 +1,19 @@
+package com.example.licensingformtest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/contact")
+public class FTcontroller {
+
+
+    @GetMapping("/formTest")
+    public String addformTest(Model model) {
+
+        return "formTest";
+    }
+
+}
---
+++ src/main/java/com/example/licensingformtest/model/app.java
@@ -0,0 +1,76 @@
+package com.example.licensingformtest.model;
+
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Date;
+
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class app {
+
+    @NotEmpty (message = "mandatory")
+    @Size(min=3 , max = 64 , message = "First Name must be in between 3 to 64 characters")
+    private String name;
+
+    @Email(message= "Invalid email format yo yo")
+    @NotBlank(message="Please fill it in") //if u leave blank, it is an optional field
+    private String email;
+
+    //@Pattern(regexp ="\\(8|9)[0-9]{7}", message ="Invalid phone number entered")
+    @Pattern(regexp = "\\d{8}", message =" please type 8 numbers")
+    private String phoneNumber;
+
+    //(min = 10, max = 100)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Past(message ="Birth date must be past date")
+    private Date dateOfBirth;
+
+    private String id;
+
+    public app(String name, String email, String phoneNumber, Date dateOfBirth) {
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.dateOfBirth = dateOfBirth;
+        //this.id = Idgenerator.generateUniqueId();
+    }
+
+    // public static Contact fromString(String line) {
+    //     return null;
+    // }   
+
+    // public boolean Agelimit( LocalDate dateOfBirth , LocalDate currTime  ) { // put in controller
+    //     int age = Period.between(currTime, dateOfBirth).getYears();
+    //     if (age> 10 && age <100);
+    //     return true;
+    // }
+
+    
+
+    
+
+   
+
+    
+    
+   
+}
+
+ 
